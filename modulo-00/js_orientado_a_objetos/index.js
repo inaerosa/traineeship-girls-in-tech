4d446b7d6fd11aa1b92a3aad1339d1cfc17ddc1c
Inaê Rosa <inaedb.rosa@gmail.com>
2022-02-06 17:04:26 -0300

    add methods

--- modulo-00/js_orientado_a_objetos/index.js
+++ modulo-00/js_orientado_a_objetos/index.js
@@ -1,14 +1,42 @@
 class Cliente{
-    nome
-    cpf
-    agencia
-    saldo
+    nome;
+    cpf;
+}
+
+class ContaCorrente{
+    agencia;
+    _saldo = 0;
+
+    sacar(valor){
+        if (this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if (valor < 0) return;         
+        this._saldo += valor;
+    }
 }
 
 const cliente1 = new Cliente();
-cliente1.nome = "Inaê"
-cliente1.cpf = 123455
-cliente1.agencia = 1001
-cliente1.saldo = 0
+cliente1.nome = "Ricardo";
+cliente1.cpf = 123455;
 
-console.log(cliente1.nome)
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 454687431;
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.depositar(100)
+contaCorrenteRicardo.depositar(100)
+contaCorrenteRicardo.depositar(100)
+
+const valor_sacado = contaCorrenteRicardo.sacar(50)
+console.log(valor_sacado)
+
+console.log(contaCorrenteRicardo)
+
