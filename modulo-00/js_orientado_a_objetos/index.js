c944eacf17a0c20d6e3e8fa7a19fc6bded1fb45b
Inaê Rosa <inaedb.rosa@gmail.com>
2022-02-06 16:00:21 -0300

    add client class

---
+++ modulo-00/js_orientado_a_objetos/index.js
@@ -0,0 +1,14 @@
+class Cliente{
+    nome
+    cpf
+    agencia
+    saldo
+}
+
+const cliente1 = new Cliente();
+cliente1.nome = "Inaê"
+cliente1.cpf = 123455
+cliente1.agencia = 1001
+cliente1.saldo = 0
+
+console.log(cliente1.nome)
