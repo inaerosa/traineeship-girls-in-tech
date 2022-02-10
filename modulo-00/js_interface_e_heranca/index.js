0afc7a7c60b0df8ced00dfee91d3551a87f68bb7
Inaê Rosa <inaedb.rosa@gmail.com>
2022-02-10 14:17:46 -0300

    add SistemaAutenticacao

--- modulo-00/js_interface_e_heranca/index.js
+++ modulo-00/js_interface_e_heranca/index.js
@@ -1,15 +1,20 @@
-import {Cliente} from "./Cliente.js";
-import { ContaCorrente } from "./Conta/ContaCorrente.js";
-import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
-import { ContaSalario } from "./Conta/ContaSalario.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
+import { Gerente } from "./Funcionarios/Gerente.js";
+import { Diretor } from "./Funcionarios/Diretor.js";
+import { Cliente } from "./Cliente.js";
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
+const diretor = new Diretor("Rodrigo", 10000, 123455678);
+diretor.cadastrar_senha("123456");
 
-const contaCorrente = new ContaCorrente(cliente1, 1001);
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-const contaSalario = new ContaSalario(cliente1);
+const gerente = new Gerente("Ricardo", 5000, 123456788);
+gerente.cadastrar_senha("123");
 
-contaSalario.depositar(100);
-contaSalario.sacar(10);
+const cliente = new Cliente("Lais", 3123123123, "456");
 
-console.log(contaSalario)
+
+const gerenteLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorLogado = SistemaAutenticacao.login(diretor, "123456");
+const clienteLogado = SistemaAutenticacao.login(cliente, "456");
+
+console.log(gerenteLogado, diretorLogado, clienteLogado);
+
