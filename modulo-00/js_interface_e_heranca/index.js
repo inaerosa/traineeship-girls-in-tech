70c7ea88fc87b185bd0f27cf20d840537fd414ad
Inaê Rosa <inaedb.rosa@gmail.com>
2022-02-10 10:12:14 -0300

    add ContaSalario.js

--- modulo-00/js_interface_e_heranca/index.js
+++ modulo-00/js_interface_e_heranca/index.js
@@ -1,17 +1,16 @@
 import {Cliente} from "./Cliente.js";
+import { Conta } from "./Conta.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
+import { ContaSalario } from "./ContaSalario.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 
 const contaCorrente = new ContaCorrente(cliente1, 1001);
-
-contaCorrente.depositar(500);
-contaCorrente.sacar(100);
-
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+const contaSalario = new ContaSalario(cliente1);
 
-contaPoupanca.sacar(10);
+contaSalario.depositar(100);
+contaSalario.sacar(10);
 
-console.log(contaPoupanca);
-console.log(contaCorrente);
+console.log(contaSalario)
