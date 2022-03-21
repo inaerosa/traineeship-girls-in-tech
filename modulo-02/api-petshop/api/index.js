9bc719d556d4b9cdcb24cbb168f0a8f3b7fd6ed7
Inaê Rosa <inaedb.rosa@gmail.com>
2022-03-21 14:00:47 -0300

    fix: add use router

--- modulo-02/api-petshop/api/index.js
+++ modulo-02/api-petshop/api/index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const config = require('config')
+const roteador = require('./rotas/fornecedores')
 
 app.use(bodyParser.json())
+
+app.use('/api/fornecedores', roteador)
 
 app.listen(config.get('api.porta'), () => console.log('A API está funcionando'))
