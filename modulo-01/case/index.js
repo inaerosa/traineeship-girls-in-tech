5cad7dc2d2147b03fc1f63944c058d24410b71bd
Inaê Rosa <inaedb.rosa@gmail.com>
2022-04-07 13:57:31 -0300

    feat: attributes functions

--- modulo-01/case/index.js
+++ modulo-01/case/index.js
@@ -60,19 +60,58 @@ function details_price(){
 
 function totalPrice(obj){
     let field_item = obj.item
-
     const totalPrice = field_item.quantity * field_item.unitPrice
     field_item.totalPrice = totalPrice
-}  
+}   
 
 function subTotal(obj){
     let field = obj.cart
     field.subtotal = totalPrice(obj)
+    let pay = obj.payments
+    if (pay != undefined) pay.charges.subTotal = totalPrice(obj)
 }
 
 function total(obj){
-    const field = obj.cart
     const item = details_price();
-    const finalTotal = item[1] - item[0]
-    field.total = finalTotal;
+    const field = obj.cart
+
+    if(field != undefined){
+        const finalTotal = item[1] - item[0]
+         field.total = finalTotal;    
+    }
+    let pay = obj.payments
+    if (pay != undefined){
+        const finalTotal = item[1] - item[0]
+        pay.charges.total = finalTotal
+    }
+}
+
+function total_discounts(obj){
+   const item = details_price()
+   obj.payments.charges.totalDiscounts = item[0]
+}
+
+function value_(obj){
+    const fields = obj.cart;
+    let value = Number((fields.total*0.33).toFixed(2));
+    newObj.payments.payment[0].value = value
+    newObj.payments.payment[1].value = Number((fields.total - value).toFixed(2));
+}
+
+
+function changeFor(obj){
+    let obj_1 = obj.payments.payment[0];
+    if (obj_1.method == "CASH") obj_1.changeFor = newObj.payments.payment[0].value + 0.07;
+    else obj_1.changeFor = 0;
+}
+
+function totalInCash(obj){
+    let payments = obj.payments.payment
+    let tot = payments[0]. value + payments[1].value;
+    newObj.payments.charges.totalInCash = tot;
+}
+
+function change(){
+    let tot = newObj.payments.payment[0].changeFor + newObj.payments.payment[1].changeFor
+    if (newObj.payments.charges != undefined) newObj.payments.charges.change = tot;
 }
