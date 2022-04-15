85c035c2c052430ffdbb53f277e2a0ba9bb7ec23
Inaê Rosa <inaedb.rosa@gmail.com>
2022-04-15 01:20:30 -0300

    feat: classe 03

--- modulo-02/controle-versoes-api/api/rotas/fornecedores/produtos/index.js
+++ modulo-02/controle-versoes-api/api/rotas/fornecedores/produtos/index.js
@@ -1,11 +1,15 @@
 const roteador = require('express').Router({mergeParams: true})
 const Tabela = require('./TabelaProduto')
 const Produto = require('./Produto')
+const Serializador = require('../../../Serializador').SerializadorProduto
 
 roteador.get('/', async (req, res) => {
     const produtos = await Tabela.listar(req.fornecedor.id)
+    const serializador = new Serializador(
+        res.getHeader('Content-Type')
+    )
     res.send(
-        JSON.stringify(produtos)
+        serializador.serializar(produtos)
     )
 })
 
@@ -16,8 +20,11 @@ roteador.post('/', async (req, res, next) => {
         const dados = Object.assign({}, body, {fornecedor: idFornecedor})
         const produto = new Produto(dados)
         await produto.criar()
+        const serializador = new Serializador(
+            res.getHeader('Content-Type')
+        )
         res.status(201)
-        res.send(produto)
+        res.send(serializador.serializar(produto))
     } catch (e){
         next(e)
     }
@@ -33,6 +40,65 @@ roteador.delete('/:idProduto', async(req, res) => {
     await produto.apagar()
     res.status(204)
     res.end()
+})
+
+roteador.get('/:id', async(req, res, next) => {
+    try{
+        const dados  = {
+            id: req.params.id,
+            fornecedor: req.fornecedor.id
+        }
+        const produto = new Produto(dados)
+        await produto.carregar()
+        const serializador = new Serializador(
+            res.getHeader('Content-Type', ['preco','estoque', 'fornecedor', 'dataCriacao', 'dataAtualizacao', 'versao'])
+        )
+       
+        res.send(
+            JSON.stringify(serializador.serializar(produto))
+        )
+    }catch(e){
+        next(e)
+    }  
+})
+
+roteador.put('/:id', async(req, res, next) => {
+    try{
+        const dados = Object.assign(
+            {},
+            req.body, 
+            {
+                id: req.params.id,
+                fornecedor: req.params.fornecedor.id
+            }
+        )
+        const produto = new Produto(dados)
+        await produto.atualizar()
+        res.status(204)
+        res.end()
+    } catch(e){
+        next(e)
+    }
+    
+
+
+})
+
+roteador.post('/:id/diminuir-estoque', async(req, res, next) => {
+    try{
+        const produto = new Produto({
+            id: req.params.id,
+            fornecedor: req.fornecedor.id
+        })
+    
+        await produto.carregar()
+        produto.estoque = produto.estoque - req.body.quantidade
+        await produto.diminuirEstoque();
+        res.status(204)
+        res.end()
+    } catch(e){
+        next(e)
+    }
 })
 
 module.exports = roteador;
