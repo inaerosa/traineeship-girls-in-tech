f3373e33877e05e45c2a5497fca0fe3506dca93f
Inaê Rosa <inaedb.rosa@gmail.com>
2022-04-19 01:19:52 -0300

    feat: create model Establishment

---
+++ modulo-02/case02/model/Establishment.js
@@ -0,0 +1,13 @@
+export class Establishment {
+    constructor (name, opening_hours, latitude, longitude, avg_ticket, total_employees, region, status, matriz){
+        this.name = name,
+        this.opening_hours = opening_hours,
+        this.latitude = latitude,
+        this.longitude = longitude, 
+        this.avg_ticket = avg_ticket,
+        this.total_employees = total_employees,
+        this.region = region, 
+        this.status = status, 
+        this.matriz = matriz
+    }
+}
