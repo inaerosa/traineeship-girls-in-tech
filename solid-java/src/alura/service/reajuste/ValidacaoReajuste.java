6c0e661d5bf031467b6d92f808d33f86f4c3be38
Inaê Rosa <inaedb.rosa@gmail.com>
2022-04-26 13:47:54 -0300

    Delete solid-java/src/alura directory

--- solid-java/src/alura/service/reajuste/ValidacaoReajuste.java
+++
@@ -1,9 +0,0 @@
-package alura.service.reajuste;
-
-import java.math.BigDecimal;
-
-import alura.model.Funcionario;
-
-public interface ValidacaoReajuste {
-	void validar (Funcionario funcionario, BigDecimal aumento);
-}
