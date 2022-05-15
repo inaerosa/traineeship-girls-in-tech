3554c83d2e16b2a6fe4d669a2c0318ba1fc853c0
Vincenzo G Neto <vincenzovgn@gmail.com>
2022-05-15 10:05:15 -0300

    sugestão de melhoria

--- modulo-02/case02/api/controller/merchantController.js
+++ modulo-02/case02/api/controller/merchantController.js
@@ -1,99 +1,99 @@
-const data = require('./../data.json')
-let establishments = data
-const Exception = require('./../errors/Exception')
+const data = require("./../data.json");
+let establishments = data;
+const Exception = require("./../errors/Exception");
+const fetchIndexByMerchant = (index) => establishments.findIndex((merchant) => merchant.id == Number(index));
+class merchantController {
+  static showAll(req, res) {
+    return res.json(establishments);
+  }
 
-class merchantController{
-    static showAll(req, res){
-        return res.json(establishments)
-    }
+  static getBranch(req, res) {
+    let filtrered = establishments.filter((merchant) => merchant.branch == true);
+    return res.json(filtrered);
+  }
 
-    static getBranch(req, res){
-        let filtrered = establishments.filter(merchant => merchant.branch == true)
-        return res.json(filtrered)
-    }
+  static getHeadOffice(req, res) {
+    var filtrered = establishments.filter((merchant) => merchant.branch == false);
+    return res.json(filtrered);
+  }
 
-    static getHeadOffice(req, res){
-        var filtrered = establishments.filter(merchant => merchant.branch == false)
-        return res.json(filtrered)
+  static getMerchantById(req, res) {
+    try {
+      const { id } = req.params;
+      const establhisment = establishments.find((establhisment) => establhisment.id == id);
+      if (establhisment == undefined) {
+        throw Exception.notFound("Establhisment not found");
+      }
+      res.status(200).json(establhisment);
+    } catch (err) {
+      res.send(err.status).json(err.message);
     }
+  }
 
-    static getMerchantById(req, res){
-        try{
-            const {id} = req.params;
-            const establhisment = establishments.find(establhisment => establhisment.id == id)
-            if (establhisment == undefined){
-                throw Exception.notFound('Establhisment not found')
-             }
-            res.status(200).json(establhisment)
-        } catch(err){
-            res.send(err.status).json(err.message)
-        }
+  static postMerchant(req, res) {
+    try {
+      let establishment = req.body;
+      establishments.push(establishment);
+      res.status(200).send(establishment);
+    } catch (err) {
+      res.status(500).send({ message: `${err.message} - Error to POST establhisment` });
     }
+  }
 
-    static postMerchant(req,res){
-        try{
-            let establishment = req.body;
-            establishments.push(establishment)
-            res.status(200).send(establishment)
-        } catch(err){
-            res.status(500).send({message: `${err.message} - Error to POST establhisment`})
-        }
+  static updateStatus(req, res) {
+    try {
+      const { id } = req.params;
+      const index = fetchIndexByMerchant(id);
+      const { status } = req.body;
+      if (index == -1) {
+        throw Exception.notFound("ID not found");
+      }
+      if (status != "OPEN" && status != "CLOSED") {
+        throw Exception.invalidData("Status must be OPEN or CLOSED");
+      }
+      return res.json((establishments[index].status = status));
+    } catch (err) {
+      return res.status(err.status).send({ message: err.message });
     }
+  }
 
-    static updateStatus(req, res){
-        try{
-            const {id} = req.params;
-            const index = establishments.findIndex(merchant => merchant.id == Number(id))
-            const {status} = req.body            
-            if(index == -1){
-                throw Exception.notFound('ID not found')
-            }
-            if (status != 'OPEN' && status != 'CLOSED'){
-                throw Exception.invalidData('Status must be OPEN or CLOSED')
-            }
-            return res.json(establishments[index].status = status)
-        } catch(err){
-            return res.status(err.status).send({message: err.message})
-        }
+  static deleteMerchant(req, res) {
+    try {
+      const { id } = req.params;
+      const index = fetchIndexByMerchant(id);
+      if (index == -1) {
+        throw Exception.notFound("ID not found");
+      }
+      establishments.splice(index, 1);
+      return res.status(200).send({ message: `Success to delete establishment` });
+    } catch (err) {
+      return res.status(err.status).send({ message: err.message });
     }
+  }
 
-    static deleteMerchant(req, res){
-        try{
-            const {id} = req.params;
-            const index = establishments.findIndex(merchant => merchant.id == Number(id))
-            if (index == -1){
-               throw Exception.notFound('ID not found')
-            }
-            establishments.splice(index, 1);
-            return res.status(200).send({message: `Success to delete establishment`}) 
-        } catch(err){
-            return res.status(err.status).send({message: err.message})
-        }
+  static sendMessage(req, res) {
+    try {
+      const { id } = req.params;
+      const { message } = req.body;
+      const index = fetchIndexByMerchant(id);
+      if (index == -1) {
+        throw Exception.notFound("ID not found");
+      }
+      const merchant = establishments[index];
+      const branch = establishments.find((establhisment) => establhisment.id == merchant.branchId);
+      if (branch == undefined) {
+        throw Exception.notFound("Establhisment not found");
+      }
+      const date = {
+        message,
+        date: new Date().toISOString(),
+      };
+      branch.messages.push(date);
+      return res.status(200).json(branch.messages);
+    } catch (err) {
+      return res.status(err.status).send({ message: err.message });
     }
-
-    static sendMessage(req, res){
-        try{
-            const {id} = req.params;
-            const {message} = req.body;
-            const index = establishments.findIndex(merchant => merchant.id == Number(id))
-            if(index == -1){
-                throw Exception.notFound('ID not found')
-            }
-            const merchant = establishments[index]
-            const branch = establishments.find(establhisment => establhisment.id == merchant.branchId)
-            if (branch == undefined){
-                throw Exception.notFound('Establhisment not found')
-             }
-            const date = {
-                message,
-                date: new Date().toISOString()
-            }
-            branch.messages.push(date)
-            return res.status(200).json(branch.messages)
-        }catch(err){
-            return res.status(err.status).send({message: err.message})
-        }
-    }
+  }
 }
 
-module.exports = merchantController
+module.exports = merchantController;
--- modulo-02/case02/api/routes/merchantRoutes.js
+++ modulo-02/case02/api/routes/merchantRoutes.js
@@ -1,13 +1,19 @@
-const router = require('express').Router()
-const merchantController = require('./../controller/merchantController')
+const router = require("express").Router();
+const merchantController = require("./../controller/merchantController");
 
-router.get('/', merchantController.showAll)
-      .get('/branch', merchantController.getBranch)
-      .get('/headOffice', merchantController.getHeadOffice)
-      .get('/:id', merchantController.getMerchantById)
-      .post('/', merchantController.postMerchant)
-      .patch('/:id/status', merchantController.updateStatus)
-      .delete('/:id', merchantController.deleteMerchant)
-      .post('/:id/send-message', merchantController.sendMessage)
+router
+  .get("/", merchantController.showAll)
+  .get("/branch", merchantController.getBranch)
+  .get("/headOffice", merchantController.getHeadOffice)
+  .get("/:id", merchantController.getMerchantById)
+  .post("/", merchantController.postMerchant)
+  .patch("/:id/status", merchantController.updateStatus)
+  .delete("/:id", merchantController.deleteMerchant)
+  .post("/:id/send-message", merchantController.sendMessage);
 
-module.exports = router
+module.exports = router;
+
+/**
+ * a rota headOffice esta em camelCase, já a rota send-message está em quebec-case.
+ * Sugiro definir um unico padrão para as rotas, de preferencia quebec-case
+ */
